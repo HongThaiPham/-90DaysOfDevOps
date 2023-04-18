a2069a232601d7a7e0ad1f9d2556a54d184c31b8
leopham <hongthaipro@gmail.com>
2023-04-18 21:37:57 +0700

    done day 11

--- Day11/main.go
+++ Day11/main.go
@@ -4,5 +4,11 @@ import "fmt"
 
 func main() {
 	var challenge = "#90DaysOfDevOps"
+	const daystotal = 90
+	var dayscomplete = 11
+
 	fmt.Println("Welcome to", challenge, "")
+	fmt.Println("This is a", daystotal, "challenge and you have completed", dayscomplete, "days")
+	fmt.Printf("This is a %v challenge and you have completed %v days\n", daystotal, dayscomplete)
+	fmt.Println("Great work")
 }
